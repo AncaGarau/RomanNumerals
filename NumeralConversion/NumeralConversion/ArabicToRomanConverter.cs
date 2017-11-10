a3a954e7ad6528dba70177fb3057be38f1f1244d
agarau@isoftwaresystems.com <sainmciang.a>
2017-11-10 19:25:20 +0200

    [TASK-1] Implement arabic to roman numeral converter

---
+++ NumeralConversion/NumeralConversion/ArabicToRomanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace NumeralConversion
+{
+	public class ArabicToRomanConverter : INumeralConverter<int, string>
+	{
+		public string Convert(int input)
+		{
+			var romanNumeralBuilder = new StringBuilder();
+
+			foreach (var romanSymbol in RomanSymbolsProvider.GetRomanSymbols)
+			{
+				var timesToAppend = input / romanSymbol.Key;
+				input = input - timesToAppend * romanSymbol.Key;
+				var currentNumeral = string.Concat(Enumerable.Repeat(romanSymbol.Value, timesToAppend));
+				romanNumeralBuilder.Append(currentNumeral);
+			}
+
+			return romanNumeralBuilder.ToString();
+		}
+	}
+}
