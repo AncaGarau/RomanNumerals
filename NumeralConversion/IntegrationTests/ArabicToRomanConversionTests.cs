b7d0cc1c4f724a6997580131d539654b7c7531b2
agarau@isoftwaresystems.com <sainmciang.a>
2017-11-12 14:10:24 +0200

    [TASK-2] Integration test cases

--- NumeralConversion/IntegrationTests/ArabicToRomanConversionTests.cs
+++ NumeralConversion/IntegrationTests/ArabicToRomanConversionTests.cs
@@ -22,6 +22,9 @@ namespace IntegrationTests
 		[TestCase("Consectetur 5 adipiscing elit 9.", "Consectetur V adipiscing elit IX.", 2)]
 		[TestCase("Lorem ipsum 2 dolor sit amet.", "Lorem ipsum II dolor sit amet.", 1)]
 		[TestCase("12345 abc1 67!", "12345 abc1 LX VII!", 1)]
+		[TestCase("Should replace 2080 with roman numeral 2080.", "Should replace MM LXXX with roman numeral MM LXXX.", 2)]
+		[TestCase("...309...309309!", "...CCC IX...309309!", 1)]
+		[TestCase("6/11/63/2222", "VI/X I/LX III/MM CC XX II", 4)]
 		public void Should_substitute_integers_between_1_and_3999_with_roman_numerals(string inputText, string expectedOutputText, int expectedNumberOfSubstitutions)
 		{
 			var result = substitutionService.Substitute(inputText);
